88b104695da8dbff61ff2858fbb0b28925eca066
Alex Ozerin <m12sl@yandex.ru>
2019-02-05 12:22:18 +0300

    restructured project, isolate webui and worker apps

---
+++ worker_one/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import logging
+import sys
+app = Flask(__name__)
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+@app.route('/vectorize', methods=['POST'])
+def vectorize():
+    # request should be json with data from web-ui
+    logger.debug(request.get_json())
+    data = request.get_json()
+    return jsonify(tokenized_text=data.get('text', 'Smth wrong').split(), attention_map=[0.9, 0.1])
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
