5de96db7b66ae3cf15e5895947d8a768d26a4d0e
sdll <shura.sdll@gmail.com>
2019-02-06 14:23:52 +0300

    add sentiment-classifier and refactor the code

--- webapp/main.py
+++ webapp/main.py
@@ -7,8 +7,8 @@ import sys
 
 def _parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', default=8000)
-    parser.add_argument('--host', default='0.0.0.0')
+    parser.add_argument("--port", default=8000)
+    parser.add_argument("--host", default="0.0.0.0")
     return parser.parse_args()
 
 
@@ -18,26 +18,41 @@ logger.setLevel(logging.DEBUG)
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-# todo: make all demos generic
-@app.route('/worker_demo')
-def worker_demo():
-    return render_template('worker_demo.html')
+@app.route("/classify-sentiment")
+def classify_sentiment():
+    return render_template("lingofunk-classify-sentiment.html")
 
 
-# todo: make worker_one generic argument and redirect blindly
-@app.route('/api/worker_one/vectorize', methods=['POST'])
-def api_worker():
-    msg = 'API proxy got {} resend to worker'.format(request.get_json())
+@app.route("/api/classifier/activations")
+def activations_api():
+    msg = f"Got {request.get_json()}, resent to the worker."
     logger.debug(msg)
     response = requests.post(
-        'http://worker_one:8000/vectorize',
-        json=request.get_json())
+        "http://sentiment-classifier:8000/activations", json=request.get_json()
+    )
     return Response(response.content, response.status_code)
+
+
+# # todo: make all demos generic
+# @app.route('/worker_demo')
+# def worker_demo():
+#     return render_template('worker_demo.html')
+
+
+# # todo: make worker_one generic argument and redirect blindly
+# @app.route('/api/worker_one/vectorize', methods=['POST'])
+# def api_worker():
+#     msg = 'API proxy got {} resend to worker'.format(request.get_json())
+#     logger.debug(msg)
+#     response = requests.post(
+#         'http://worker_one:8000/vectorize',
+#         json=request.get_json())
+#     return Response(response.content, response.status_code)
 
 
 if __name__ == "__main__":
