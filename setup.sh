dcd1bd99cc2f6ab216f88cba771f7742d9f62bc4
sdll <shura.sdll@gmail.com>
2019-02-07 19:44:31 +0300

    add the setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
+# sync the prerequisite files
+function sync() {
+    local -n files=$1
+    for file in "${files[@]}"; do
+        mkdir -p $(dirname $file)
+        [ -f ../$file ] && cp -n ../$file $file
+    done
+}
+
 WEIGHTS=lingofunk-classify-sentiment/lingofunk_classify_sentiment/assets/model/hnatt/weights.h5
 EMBEDDING=lingofunk-classify-sentiment/lingofunk_classify_sentiment/assets/embedding/glove.840B.300d.txt
-cp ../$WEIGHTS $WEIGHTS
-cp ../$EMBEDDING $EMBEDDING
+
+paths=($WEIGHTS $EMBEDDING)
+
+sync paths
+
+# sync the submodules
 git submodule update --recursive --remote --init
 git pull
